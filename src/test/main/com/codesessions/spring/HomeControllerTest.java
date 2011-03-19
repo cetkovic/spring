0ed5ff7e381876f8f864e277b29f094d18ffaf1d
Vladimir Cetkovic <vladimir.cetkovic@gmail.com>
2011-03-19 18:15:36 +0100

    added test dir, removed locally biased dirs

---
+++ src/test/main/com/codesessions/spring/HomeControllerTest.java
@@ -0,0 +1,18 @@
+package com.codesessions.spring;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import junit.framework.TestSuite;
+
+/**
+ * @author Vladimir Cetkovic
+ * @version 1.0
+ */
+public class HomeControllerTest extends TestSuite {
+	
+	@Test
+	public void test1() {
+		Assert.assertNull(null);
+	}
+}
